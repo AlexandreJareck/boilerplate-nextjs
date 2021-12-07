e6e7dc6f08f2f0ee2735b56c83be65c5f00d470d
Alexandre Jareck <xandysjp@gmail.com>
2021-12-07 09:01:03 -0300

    Configurações finais (storybook e babel)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,16 @@
+const path = require('path');
+
 module.exports = {
-  stories: ['../src/components/**/stories.tsx'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-actions',
-  ]
+  webpackFinal: async (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, '../src');
+    return config;
+  },
+  "stories": [
+    "../src/components/**/stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "framework": "@storybook/react",
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ function App({ Component, pageProps }: AppProps) {
         <title>Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="appple-touch icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple project starter to work with ts, reactm nextjs and style components"
